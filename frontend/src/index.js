b77e50d218f16fd5f8901c859238377303310446
Derek Roode <droode@nexient.com>
2021-12-29 16:41:54 -0500

    added dependencies and suspense wrapper

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import { React, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
@@ -12,7 +12,9 @@ const store = configureStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Suspense fallback="... is loading">
+        <App />
+      </Suspense>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
