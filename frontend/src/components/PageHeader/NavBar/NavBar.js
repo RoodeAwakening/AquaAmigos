3d4231b882c3a610441b36fa75807fafd0f61835
Derek Roode <droode@nexient.com>
2022-01-10 23:17:12 -0500

    name change

--- frontend/src/components/PageHeader/NavBar/NavBar.js
+++ frontend/src/components/PageHeader/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import i18next from 'i18next';
 import React from 'react';
-import MembersOnly from '../../security/MembersOnly/MembersOnly';
+import MembersOnly from '../../securityRoutes/MembersOnly/MembersOnly';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
