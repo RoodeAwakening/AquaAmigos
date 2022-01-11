c0c5fe32ef312c9c97b526adfbfac63b5ae932f6
Derek Roode <droode@nexient.com>
2022-01-10 21:40:27 -0500

    updated rendering for events in carousel

--- frontend/src/components/Home/Carousel/Carousel.js
+++ frontend/src/components/Home/Carousel/Carousel.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Slider from 'react-slick';
+import i18next from 'i18next';
+import { NavLink } from 'react-router-dom';
 import styles from './Carousel.module.css';
 import mockEvent from '../../../mocks/Event';
 
@@ -30,20 +32,21 @@ function Carousel() {
   };
 
   const renderEvents = () => {
-    return Object.keys(events).map((key) => {
+    // return the 3 most recent events
+    return Object.keys(events).slice(0, 3).map((key) => {
       const event = events[key];
       return (
         <div key={event.id} className={styles.carousel_event_container}>
           <div className={styles.carousel_event_image_container}>
-            <img src={event.image} alt="event" />
+            <img src={event.coverImage} alt="event" />
           </div>
           <div className={styles.carousel_event_info_container}>
             <div className={styles.carousel_event_title}>{event.title}</div>
             <div className={styles.carousel_event_description}>{event.description}</div>
             <div className={styles.carousel_event_button}>
-              <button type="button">
-                View Event
-              </button>
+              <NavLink to={`events/${event.id}`}>
+                {i18next.t('msg_event_view')}
+              </NavLink>
             </div>
 
           </div>
