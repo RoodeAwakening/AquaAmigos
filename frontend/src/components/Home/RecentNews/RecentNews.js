0092e2c7e7393c6a70c4c7ae0bde85dd0d04ee3d
Derek Roode <droode@nexient.com>
2022-01-24 21:38:50 -0500

    fixed the issues with recent news articles

--- frontend/src/components/Home/RecentNews/RecentNews.js
+++ frontend/src/components/Home/RecentNews/RecentNews.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import i18next from 'i18next';
 import ContentBlock from '../../common/ContentBlock/ContentBlock';
 import styles from './RecentNews.module.css';
 import NewsArticles from '../../../mocks/NewsArticles';
@@ -24,9 +25,17 @@ function RecentNews() {
   return (
     <div className={styles.recentNews_container}>
       { NewsArticles.length && (
-        <>
-          {renderNews()}
-        </>
+        <div>
+          <div className={styles.recentNews__titles}>
+            <h1>{i18next.t('msg_news_header')}</h1>
+            <h4>{i18next.t('msg_news_sub')}</h4>
+          </div>
+          <div className={styles.recentNews__articles}>
+            <div className={styles.about_us__members_alignment}>
+              {renderNews()}
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
